a466ca23d9fb1686dee43d9f7708679e2b1ea0e6
over.light <over.light888@gmail.com>
2022-11-22 16:50:50 +0530

    completely added backend sewrvices

--- src/server/trpc/router/commentsRouter.ts
+++ src/server/trpc/router/commentsRouter.ts
@@ -10,6 +10,36 @@ const commentsRouter = router({
     });
     return items;
   }),
+  getPostById: publicProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .query(async ({ ctx, input }) => {
+      const item = await ctx.prisma.webDevPost.findUnique({
+        where: { id: input.id },
+        select: {
+          body: true,
+          title: true,
+          comments: {
+            orderBy: {
+              createdAt: "desc",
+            },
+            select: {
+              id: true,
+              message: true,
+              parentId: true,
+              createdAt: true,
+
+              user: {
+                select: {
+                  id: true,
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      });
+      return item;
+    }),
 });
 
 export { commentsRouter };
